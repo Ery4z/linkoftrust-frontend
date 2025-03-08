d7806390dc10bd0c1b79751c8a2f6304ae3fd475
Thomas Bolteau <thomas.bolteau50@gmail.com>
2025-03-08 22:15:16 +0000

    ignoring error for warn

--- src/app/HeaderMenu.tsx
+++ src/app/HeaderMenu.tsx
@@ -4,72 +4,72 @@
 import React from "react";
 import { Button, Container, Title, Text } from "@mantine/core";
 import { useWallet } from "@/context/WalletContext";
+import Link from "next/link";
 
 export function HeaderMenu() {
-  // Extract the network and authentication functions from your wallet context.
-  const { accountId, network, signIn, signOut } = useWallet();
+    // Extract the network and authentication functions from your wallet context.
+    const { accountId, network, signIn, signOut } = useWallet();
 
-  // Compute the redirect URL based on the current network.
-  const redirectUrl =
-    network === "mainnet"
-      ? process.env.NEXT_PUBLIC_TESTNET_SITE_URL || "https://testnet.linkoftrust.org"
-      : process.env.NEXT_PUBLIC_MAINET_SITE_URL || "https://mainnet.linkoftrust.org";
+    // Compute the redirect URL based on the current network.
+    const redirectUrl =
+        network === "mainnet"
+            ? process.env.NEXT_PUBLIC_TESTNET_SITE_URL || "https://testnet.linkoftrust.org"
+            : process.env.NEXT_PUBLIC_MAINET_SITE_URL || "https://mainnet.linkoftrust.org";
 
-  return (
-    <header>
-      {/* Top Header: Title and authentication */}
-      <div className="bg-neutral-900 text-gray-100 py-4 shadow">
-        <Container fluid>
-          <div className="flex justify-between items-center">
-            <div className="flex items-center space-x-2">
-              <a href="/" className="no-underline text-gray-100 hover:text-white">
-                <Title order={3}>Link Of Trust</Title>
-              </a>
+    return (
+        <header>
+            {/* Top Header: Title and authentication */}
+            <div className="bg-neutral-900 text-gray-100 py-4 shadow">
+                <Container fluid>
+                    <div className="flex justify-between items-center">
+                        <div className="flex items-center space-x-2">
+                            <Link href="/" className="no-underline text-gray-100 hover:text-white">
+                                <Title order={3}>Link Of Trust</Title>
+                            </Link>
+                        </div>
+
+
+                        <div className="flex items-center space-x-3">
+                            {accountId ? (
+                                <>
+                                    <Text color="yellow">Hi, {accountId}</Text>
+                                    <Button onClick={signOut} size="xs" variant="light">
+                                        Sign Out
+                                    </Button>
+                                </>
+                            ) : (
+                                <Button onClick={signIn} size="xs" variant="light">
+                                    Sign In
+                                </Button>
+                            )}
+                        </div>
+                    </div>
+                </Container>
             </div>
-            
 
-            <div className="flex items-center space-x-3">
-              {accountId ? (
-                <>
-                  <Text color="yellow">Hi, {accountId}</Text>
-                  <Button onClick={signOut} size="xs" variant="light">
-                    Sign Out
-                  </Button>
-                </>
-              ) : (
-                <Button onClick={signIn} size="xs" variant="light">
-                  Sign In
-                </Button>
-              )}
-            </div>
-          </div>
-        </Container>
-      </div>
-
-      {/* Sub Bar: Network status and redirect button */}
-      <div
-        className={`${
-          network === "mainnet" ? "bg-neutral-800" : "bg-orange-500"
-        } text-white py-2  lg:w-1/3 rounded-br-xl`}
-      >
-        <Container fluid>
-          <div className="flex justify-between items-center h-full ">
-            {/* Display the current network */}
-            <span className="font-medium">
-              {network === "mainnet" ? "Mainnet" : "Testnet"}
-            </span>
-            {/* Button that redirects to the opposite network subdomain */}
-            <Button
-              component="a"
-              href={redirectUrl}
-              size="xs"
-              variant="light"
+            {/* Sub Bar: Network status and redirect button */}
+            <div
+                className={`${network === "mainnet" ? "bg-neutral-800" : "bg-orange-500"
+                    } text-white py-2  lg:w-1/3 rounded-br-xl`}
             >
-              Switch to {network === "mainnet" ? "Testnet" : "Mainnet"}
-            </Button>
-          </div>
-        </Container>
-      </div>
-    </header>
-  );
+                <Container fluid>
+                    <div className="flex justify-between items-center h-full ">
+                        {/* Display the current network */}
+                        <span className="font-medium">
+                            {network === "mainnet" ? "Mainnet" : "Testnet"}
+                        </span>
+                        {/* Button that redirects to the opposite network subdomain */}
+                        <Button
+                            component="a"
+                            href={redirectUrl}
+                            size="xs"
+                            variant="light"
+                        >
+                            Switch to {network === "mainnet" ? "Testnet" : "Mainnet"}
+                        </Button>
+                    </div>
+                </Container>
+            </div>
+        </header>
+    );
 }
--- src/app/explore/ColoredEdge.tsx
+++ src/app/explore/ColoredEdge.tsx
@@ -5,53 +5,53 @@ import { BaseEdge, EdgeProps, getSmoothStepPath, MarkerType } from "@xyflow/reac
 import { SelectedNodeContext } from "./SelectedNodeContext";
 
 export default function ColoredEdge(props: EdgeProps) {
-  const { id, source, target, sourceX, sourceY, targetX, targetY, sourcePosition, targetPosition } = props;
-  const [edgePath, labelX, labelY] = getSmoothStepPath({
-    sourceX,
-    sourceY,
-    sourcePosition,
-    targetX,
-    targetY,
-    targetPosition,
-  });
+    const { id, source, target, sourceX, sourceY, targetX, targetY, sourcePosition, targetPosition } = props;
+    const [edgePath, _, __] = getSmoothStepPath({
+        sourceX,
+        sourceY,
+        sourcePosition,
+        targetX,
+        targetY,
+        targetPosition,
+    });
 
-  // You can also read from your context if needed:
-  const { selectedNodeId } = useContext(SelectedNodeContext);
-  
-  // Default style (fallback)
-  let strokeColor = "#555";
-  let strokeWidth = 2;
+    // You can also read from your context if needed:
+    const { selectedNodeId } = useContext(SelectedNodeContext);
 
-  // If this edge’s target = selected node => e.g. show orange
-  if (target === selectedNodeId) {
-    strokeColor = "#DD6E42"; // trusting color
-    strokeWidth = 3;
-  }
-  // If this edge’s source = selected node => e.g. show blue
-  else if (source === selectedNodeId) {
-    strokeColor = "#96daf4"; // trusted color
-    strokeWidth = 3;
-  }
+    // Default style (fallback)
+    let strokeColor = "#555";
+    let strokeWidth = 2;
 
-  const animation = (target === selectedNodeId || source === selectedNodeId) &&(
-    <circle r="5" fill={strokeColor}>
-        <animateMotion dur="2s" repeatCount="indefinite" path={edgePath} />
+    // If this edge’s target = selected node => e.g. show orange
+    if (target === selectedNodeId) {
+        strokeColor = "#DD6E42"; // trusting color
+        strokeWidth = 3;
+    }
+    // If this edge’s source = selected node => e.g. show blue
+    else if (source === selectedNodeId) {
+        strokeColor = "#96daf4"; // trusted color
+        strokeWidth = 3;
+    }
+
+    const animation = (target === selectedNodeId || source === selectedNodeId) && (
+        <circle r="5" fill={strokeColor}>
+            <animateMotion dur="2s" repeatCount="indefinite" path={edgePath} />
         </circle>
-  )
-  const markerEnd = {type: MarkerType.ArrowClosed, width:20, height:20, color:strokeColor};
-  return (
-    <>
-      <BaseEdge
-        id={id}
-        path={edgePath}
-        style={{ stroke: strokeColor, strokeWidth }}
-        markerEnd={markerEnd as any}
-        // markerStart={markerStart}
-        // markerEnd={type:MarkerType.ArrowClosed}
-          // We'll define this in the SVG below
-      />
-      {animation}
-      {/* You could add an animated circle or other decorations if desired */}
-    </>
-  );
+    )
+    const markerEnd = { type: MarkerType.ArrowClosed, width: 20, height: 20, color: strokeColor };
+    return (
+        <>
+            <BaseEdge
+                id={id}
+                path={edgePath}
+                style={{ stroke: strokeColor, strokeWidth }}
+                markerEnd={markerEnd as any}
+            // markerStart={markerStart}
+            // markerEnd={type:MarkerType.ArrowClosed}
+            // We'll define this in the SVG below
+            />
+            {animation}
+            {/* You could add an animated circle or other decorations if desired */}
+        </>
+    );
 }
