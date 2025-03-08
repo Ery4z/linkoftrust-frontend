7a2ccbe8d870af082185c26ccbaffcf34bf24b5a
Thomas Bolteau <thomas.bolteau50@gmail.com>
2025-03-08 22:23:58 +0000

    ignoring error for warn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // there are ESLint warnings or errors. Make sure you use this option carefully.
+        ignoreDuringBuilds: true,
+      },
 };
 
 export default nextConfig;
